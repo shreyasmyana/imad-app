b1196bbc9d0ca6e6c31bcfadf1c2a174f6f98972
shreyasmyana <31059819+shreyasmyana@users.noreply.github.com>
2017-08-18 16:59:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,9 +21,13 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick=function(){
     
-
-    
-    var names=['name1','name2','name3','name4'];
+ var request = new XMLHttpRequest();
+   
+   request.onreadystatechange = function(){
+       if (request.readystate === XMLHttpRequest.DONE){
+           if(request.status===200){
+                 var names = request.responseText;
+                 names=JSON.parse(names);
     var list ='';
     for(var i=0;i<names.length;i++)
     {
@@ -31,6 +35,14 @@ submit.onclick=function(){
     }
     var ul= document.getElementById('namelist');
     ul.innerHTML=list;
+             
+           }
+       }
+   }; 
+      request.open('GET','http://shreyasmyana1997.imad.hasura-app.io/submit-name?name='+ name ,true);
+      request.send(null);
+    
+  
     
 };
 
