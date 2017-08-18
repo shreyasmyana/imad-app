743cfc22af33a574f861a0631c207c5f81496ecd
shreyasmyana <31059819+shreyasmyana@users.noreply.github.com>
2017-08-18 10:36:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,49 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone={
+    title:'Article-one | Shreyas Myana',
+    heading: 'article one',
+    date:'july 30',
+    content:`  <div>
+            <p>This is article one. This is article one. This is article one</p>
+            <p>This is article one. This is article one. This is article one</p>
+            <p>This is article one. This is article one. This is article one</p>
+            </div> `
+};
+function create(data)
+{
+var title =data.title;
+var heading=data.heading;
+var date=data.date;
+var content=data.content;
+var template =
+    `<html>
+<head>
+    <title> {$heading}  </title>    
+</head>
+    <body>
+        <div>
+        <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+            {$heading}
+        </h3>
+        <div> {$date}</div>
+        <div>
+            {$content}
+            </div>
+    </body>    
+</html>
+`;
+ return template;   
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));    
+  res.send(create(articleone));    
 });
 app.get('/article-two', function(req, res){
    res.send('This is article two'); 
