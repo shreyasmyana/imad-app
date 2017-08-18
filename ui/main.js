45754bb5517f54062e32a9b8ebf811bdb2a590d0
shreyasmyana <31059819+shreyasmyana@users.noreply.github.com>
2017-08-18 15:29:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,13 +2,21 @@ console.log('Loaded!');
 var button = document.getElementById('counter');
 var counter=0;
 button.onclick = function(){
+   var request = new XMLHttpRequest();
+   
+   request.onreadystatechange = function(){
+       if (request.readystate === XMLHttpRequest.DONE){
+           if(request.status===200){
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+           }
+       }
+   }; 
     
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();  
-    
-    
-}
+      request.open('GET','http://shreyasmyana1997.imad.hasura-app.io',true);
+      request.send(null);
+};
 
   
     
