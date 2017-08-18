8ac792309e00e16069ee2d59c378c64c8bf6f9f3
shreyasmyana <31059819+shreyasmyana@users.noreply.github.com>
2017-08-18 17:39:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,7 +30,7 @@ submit.onclick=function(){
     var list ='';
     for(var i=0;i<names.length;i++)
     {
-        list+='<li>'+names[i]+'</li>';
+        list +='<li>' + names[i] + '</li>';
     }
     var ul= document.getElementById('namelist');
     ul.innerHTML=list;
