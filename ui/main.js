44167a1e7e8eb3b62870199aea91ef80c3ead76c
shreyasmyana <31059819+shreyasmyana@users.noreply.github.com>
2017-08-18 15:10:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,14 @@
 console.log('Loaded!');
-var img = document.getElementById('sam');
-var marginLeft=0;
-function moveRight()
-{
-    marginLeft=marginLeft + 1;
-    img.style.marginLeft=marginLeft + 'px';
+var counter=0;
+var button = document.getElementById('counter');
+button.onclick = function(){
+    
+    counter = counter + 1;
+    var span = document.getElementById('counter');
+    span.innerHTML = counter.toString();  
+    
+    
 }
-
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
-
-};
-
 
   
     
